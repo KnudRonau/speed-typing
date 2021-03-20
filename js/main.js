94d5b14951a37001e0c5d524dc3f9a3ec8e8952d
Knud Ronau <Knudronau@hotmail.com>
2021-03-20 11:50:49 +0200

    Code checked and comments written in Javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 
-//opens the XML file
-function loadDoc() {
+//Global variables
+let variables = {
+    startTime: null,
+    timer: null,
+    index: 0,
+    errors: 0
+};
 
-    var xmlRequester = new XMLHttpRequest();
+//opens the XML file and returns it in a usable form
+function loadDoc() {
+    let xmlRequester = new XMLHttpRequest();
     xmlRequester.open("get",  "texts.xml", false);
     xmlRequester.send();
     return xmlRequester.responseXML;
@@ -23,12 +30,16 @@ function loadText(i) {
     let authorTag = xml.getElementsByTagName("author");
     let textTag = xml.getElementsByTagName("text");
 
+    //stores the desired XML-values in variables
     let title = titleTag[i].childNodes[0].nodeValue;
     let author = authorTag[i].childNodes[0].nodeValue;
     let text = textTag[i].childNodes[0].nodeValue;
+
+    //stores word and character information in variables
     let words = text.split(" ").length;
     let chars = text.length;
 
+    //adds the information to the webpage
     document.getElementById("textContent").innerHTML = text;
     document.getElementById("title").innerHTML = title;
     document.getElementById("textInformation").innerHTML = author + " (" + words + " words, " + chars + "chars)";
@@ -37,70 +48,80 @@ function loadText(i) {
 //responsible for changing language
 function changeLanguage(language) {
     let xml = loadDoc();
+    let titleTag = xml.getElementsByTagName("title");
     let i = 0;
     let j = 0;
     if(language === "english") {
-        j = j + 4;
+        j += 4;
     }
 
+    //loads each title and adds them as options on the webpage
     for(i; i < 4; i++, j++) {
-        let titleTag = xml.getElementsByTagName("title");
         let title = titleTag[j].childNodes[0].nodeValue;
         document.getElementById("option" + (i + 1)).innerHTML = title;
     }
+
+    //updates the text to match the language
     choose(document.getElementById("selectText").value);
 }
 
+//Plays sound when wrong button is clicked
 function playSound() {
     let sound = new Audio("audio/failSound.mp3");
     sound.play();
 }
 
+//Draws the process of average gross WPM
 function drawStat(time, wpm) {
     let ctx = document.querySelector("canvas").getContext("2d");
-    let xGrid = 120 * time;
 
+    //draws a continuing line, which progresses 2 pixels pr second
     ctx.strokeStyle = "#c7dff5"
-    ctx.lineTo(xGrid, 100 - parseInt(wpm));
+    ctx.lineTo(2 * time, 100 - parseInt(wpm));
     ctx.stroke();
-    console.log(wpm, time);
 }
 
+//Sets up the canvas and starts a new path to draw from
 function drawCanvas() {
     let canvas = document.querySelector("canvas");
     let ctx = canvas.getContext("2d");
+    //clears the canvas, so that if a new game is started, old stats will be wiped
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    //draws horizontal lines at 25/50/75 WPM
     let yGrid = 25;
-
     ctx.beginPath();
-
     while(yGrid<canvas.height) {
         ctx.moveTo(0, yGrid);
         ctx.lineTo(canvas.width, yGrid);
         yGrid += 25;
     }
-
     ctx.strokeStyle = "#dda544";
     ctx.stroke();
 
+    //starts a new line, which starts from the lower left corner
     ctx.beginPath();
     ctx.moveTo(0, 100);
 }
 
-function dup1() {
+//compares the last character of the input box with the reached <span> element, assigns an appropriate class and
+//increments the index
+function keyPress() {
+    //sets variables to be used in this function for at better overview.
     let text = document.getElementById("textContent").querySelectorAll("span");
-    let index = window.sessionStorage.getItem("index");
-    let nextIndex = parseInt(index) + 1;
+    let index = variables.index;
+    let nextIndex = index + 1;
     let input = document.getElementById("inputBox").value.split("");
-    let errors = parseInt(window.sessionStorage.getItem("errors"));
     let textChar = text[index].innerText;
     let inputChar = input[input.length - 1];
+
+    //if "ignore casing" is checked, both input and text character is set to lower
     if(document.getElementById("casingBox").checked) {
         textChar = textChar.toLowerCase();
         inputChar = inputChar.toLowerCase();
     }
 
+    //adds the appropriate class and removes "current" from the <span> element and possibly increments the error counter
     if(textChar === inputChar) {
         text[index].classList.add("correct");
     } else {
@@ -110,92 +131,115 @@ function dup1() {
         } else {
             text[index].classList.add("incorrect");
         }
-        window.sessionStorage.setItem("errors", errors + 1);
+        variables.errors += 1;
     }
-
     text[index].classList.remove("current");
+
+    //clears the input box when a "space" is clicked
     if(inputChar === " ") {
         document.getElementById("inputBox").value = "";
     }
 
+    //ends the game at the end or sets the next element as current and increments the index
     if(nextIndex === text.length) {
         speedTyping();
     } else {
         text[nextIndex].classList.add("current");
-        window.sessionStorage.setItem("index", nextIndex);
+        variables.index = nextIndex;
     }
 
 }
 
+
+//Turns the current text into <span> elements
 function renderText() {
     let text = document.getElementById("textContent").innerHTML;
+
+    //empties the area of the text content
     document.getElementById("textContent").innerHTML = "";
+
+    //creates an array with each character, creates a <span> element equal to each character in the array, and adds
+    //all the <span> elements to the area of text content
     text.split('').forEach(character => {
-        const characterSpan = document.createElement('span')
+        let characterSpan = document.createElement('span')
         characterSpan.innerText = character;
         document.getElementById("textContent").appendChild(characterSpan);
-
     })
 
+    //sets the first <span> element to current
     document.getElementById("textContent").querySelector("span").classList.add("current");
-
 }
 
+//Adds updated stats to the page and calls the function to draw canvas stats.
 function statistics() {
-    let grossWPMElement = document.getElementById("grossWPM");
-    let accuracyElement = document.getElementById("accuracy");
-    let netWPMElement = document.getElementById("netWPM");
-    let errorsElement = document.getElementById("errors");
 
-    let errors = window.sessionStorage.getItem("errors");
-    let entries = window.sessionStorage.getItem("index");
-    let startTime = window.sessionStorage.getItem("startTime");
+    //loads the stored values
+    let errors = variables.errors;
+    let entries = variables.index;
+    let startTime = variables.startTime;
 
-    let accuracy = (1-parseInt(errors)/parseInt(entries))*100;
+    //calculates the statistics
+    let accuracy = Math.round((1- errors/entries) * 100);
     let d = new Date();
     let currentTime = d.getTime();
-    let elapsedTime = (currentTime - startTime)/(60 * 1000);
-    let grossWPM = (entries / 5) / elapsedTime;
-    let netWPM = grossWPM - (errors/elapsedTime);
+    let elapsedTime = (currentTime - startTime)/1000;
+    let elapsedTimeMinutes = elapsedTime / 60;
+    let grossWPM = Math.round((entries / 5) / elapsedTimeMinutes);
+    let netWPM = Math.round(grossWPM - (errors/elapsedTimeMinutes));
 
+    //calls the drawStat function to update the canvas with the new stats
     drawStat(elapsedTime, grossWPM);
 
-    grossWPMElement.innerHTML = Math.round(grossWPM);
-    accuracyElement.innerHTML = Math.round(accuracy);
-    netWPMElement.innerHTML = Math.round(netWPM);
-    errorsElement.innerHTML = errors;
+    //sets the new values in the statistics area
+    document.getElementById("grossWPM").innerHTML = grossWPM;
+    document.getElementById("accuracy").innerHTML = accuracy;
+    document.getElementById("netWPM").innerHTML = netWPM;
+    document.getElementById("errors").innerHTML = errors;
+}
+
+//Sets the global values, which need to be stored between function calls.
+function setStartValues() {
+    //startTime is needed to continuously monitor how long the game has been running
+    let d1 = new Date();
+    let startTime = d1.getTime();
+    //setInterval is stored globally, so that it can be cleared later
+    let timer = setInterval(statistics, 1000);
+
+    document.getElementById("inputBox").value = "";
+
+    //index is stored globally in order to progress to the next element with each function from input, and errors for
+    //statistical purposes
+    variables.startTime = startTime;
+    variables.timer = timer;
+    variables.index = 0;
+    variables.errors = 0;
 
 }
 
+//If no game is running, this function calls all functions necessary to prepare a game and switches the image.
+// Otherwise it clears the timer, so that no further statistics is updated and switches the image back.
 function speedTyping() {
     let img = document.getElementById("startStopBtn");
-    let inputBox = document.getElementById("inputBox");
     let selectText = document.getElementById("selectText");
 
     if(img.getAttribute("src") === "img/StartButton.png") {
+        //renews the text, if the game has already been played
         choose(selectText.value);
+        //renders the text to <span> elements
         renderText();
-        inputBox.value = "";
-
-        let d1 = new Date();
-        let startTime = d1.getTime();
-        let timer = setInterval(statistics, 1000);
-
-        window.sessionStorage.setItem("timer", timer);
-        window.sessionStorage.setItem("startTime", startTime);
-        window.sessionStorage.setItem("index", 0);
-        window.sessionStorage.setItem("errors", 0);
+        //stores starting time, starts interval for statistics and resets index and errors to 0 in case the game has
+        //been played before
+        setStartValues();
+        //sets up the canvas
         drawCanvas();
 
         img.setAttribute("src", "img/StopButton.png");
-        inputBox.focus();
+        document.getElementById("inputBox").focus();
 
-    }
-    else if(img.getAttribute("src") === "img/StopButton.png") {
-
-        let timer = window.sessionStorage.getItem("timer");
+    } else if(img.getAttribute("src") === "img/StopButton.png") {
+        //stops the interval
+        let timer = variables.timer;
         clearInterval(timer);
-
 
         img.setAttribute("src", "img/StartButton.png");
     }
@@ -214,26 +258,29 @@ function addListeners() {
     document.getElementById("selectText").addEventListener("change", function () {
              choose(this.value);}, false);
 
+    //adds listener to the start button
     document.getElementById("startStopBtn").addEventListener("click", function () {
         speedTyping();}, false);
 
+    //adds listener to the input box
     document.getElementById("inputBox").addEventListener(("input"), function () {
-        dup1();}, false);
+        keyPress();}, false);
 
 
 }
 
 //runs when the page has been loaded an calls the proper functions
 function start() {
+    //loads an initial text
     choose(0);
+    //loads text options
     changeLanguage("swedish");
+    //adds eventListeners to the page
     addListeners();
+    //sets the lines in the canvas, so that they are there from when the page is opened
     drawCanvas();
-
-
-
-
 }
 
+//calls the starting function on load
 window.addEventListener("load", start, false);
 
